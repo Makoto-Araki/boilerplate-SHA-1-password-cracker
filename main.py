b17e9375680803b2245060476ec4c2b59b3df0dd
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-02-25 04:49:28 +0000

    2023-02-25 : 04

--- main.py
+++ main.py
@@ -2,12 +2,34 @@
 import password_cracker
 from unittest import main
 
+# sammy123
 cracked_password1 = password_cracker.crack_sha1_hash(
-    "fbbe7e952d1050bfb09dfdb71d4c2ff2b3d845d2")
+    "b305921a3723cd5d70a375cd21a61e60aabb84ec")
 print(cracked_password1)
 
+# abacab
+cracked_password1 = password_cracker.crack_sha1_hash(
+    "c7ab388a5ebefbf4d550652f1eb4d833e5316e3e")
+print(cracked_password1)
+
+# password
+cracked_password1 = password_cracker.crack_sha1_hash(
+    "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8")
+print(cracked_password1)
+
+# superman
 cracked_password2 = password_cracker.crack_sha1_hash(
-    "dcc466796201f7232b22a03781110a8871fd038c", use_salts=True)
+    "53d8b3dc9d39f0184144674e310185e41a87ffd5", use_salts=True)
+print(cracked_password2)
+
+# q1w2e3r4t5
+cracked_password2 = password_cracker.crack_sha1_hash(
+    "da5a4e8cf89539e66097acd2f8af128acae2f8ae", use_salts=True)
+print(cracked_password2)
+
+# bubbles1
+cracked_password2 = password_cracker.crack_sha1_hash(
+    "ea3f62d498e3b98557f9f9cd0d905028b3b019e1", use_salts=True)
 print(cracked_password2)
 
 # Run unit tests automatically
