132e66ebf43da7936dd47098dd298cd75fb817c3
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-02-25 03:38:00 +0000

    2023-02-25 : 02

--- password_cracker.py
+++ password_cracker.py
@@ -1,11 +1,27 @@
 import hashlib
 
-def crack_sha1_hash(hash):
-    with open('top-10000-passwords.txt', 'r') as file:
-        datalist = file.readlines()
+def crack_sha1_hash(hash, use_salts = False):
+    with open('top-10000-passwords.txt', 'r') as file1:
         result = 'PASSWORD NOT IN DATABASE';
-        for data in datalist:
-            if (hashlib.sha1(data.rstrip('\n').encode("utf-8")).hexdigest() == hash):
-                result = data
-                break
-        return result
+        datalist1 = file1.readlines()
+        if (use_salts == False):
+            for data1 in datalist1:
+                tmp1 = data1.rstrip('\n').encode("utf-8")
+                tmp2 = hashlib.sha1(tmp1).hexdigest()
+                if (tmp2 == hash):
+                    result = data1
+                    break
+            return result
+        else:
+            with open('known-salts.txt', 'r') as file2:
+                datalist2 = file2.readlines()
+                for data1 in datalist1:
+                    for data2 in datalist2:
+                        tmp1 = data2.rstrip('\n').encode("utf-8")
+                        tmp2 = data1.rstrip('\n').encode("utf-8")
+                        tmp3 = tmp1 + tmp2
+                        tmp4 = hashlib.sha1(tmp3).hexdigest()
+                        if (tmp4 == hash):
+                            result = data1
+                            break
+                return result
