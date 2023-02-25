97042a548ee5127cd104e4db715e03c2c8b0f792
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-02-25 04:26:35 +0000

    2023-02-25 : 03

--- password_cracker.py
+++ password_cracker.py
@@ -9,7 +9,7 @@ def crack_sha1_hash(hash, use_salts = False):
                 tmp1 = data1.rstrip('\n').encode("utf-8")
                 tmp2 = hashlib.sha1(tmp1).hexdigest()
                 if (tmp2 == hash):
-                    result = data1
+                    result = data1.rstrip('\n')
                     break
             return result
         else:
@@ -17,11 +17,11 @@ def crack_sha1_hash(hash, use_salts = False):
                 datalist2 = file2.readlines()
                 for data1 in datalist1:
                     for data2 in datalist2:
-                        tmp1 = data2.rstrip('\n').encode("utf-8")
-                        tmp2 = data1.rstrip('\n').encode("utf-8")
+                        tmp1 = data1.rstrip('\n').encode("utf-8")
+                        tmp2 = data2.rstrip('\n').encode("utf-8")
                         tmp3 = tmp1 + tmp2
                         tmp4 = hashlib.sha1(tmp3).hexdigest()
                         if (tmp4 == hash):
-                            result = data1
+                            result = data1.rstrip('\n')
                             break
                 return result
