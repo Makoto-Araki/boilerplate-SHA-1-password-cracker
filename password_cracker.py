78531cb3a82ba07e3141f9e1930d12d48759a6c2
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-02-25 02:12:06 +0000

    2023-02-25 : 01

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,11 @@
 import hashlib
 
 def crack_sha1_hash(hash):
-    
+    with open('top-10000-passwords.txt', 'r') as file:
+        datalist = file.readlines()
+        result = 'PASSWORD NOT IN DATABASE';
+        for data in datalist:
+            if (hashlib.sha1(data.rstrip('\n').encode("utf-8")).hexdigest() == hash):
+                result = data
+                break
+        return result
